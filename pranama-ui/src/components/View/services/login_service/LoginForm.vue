9db493f4742f1cf3c551bdc5dfce97c3b87bd29e
escuela-tech <escuela.tech@gmail.com>
2020-05-16 08:20:07 -0500

    Login page will accept email & password combination

--- pranama-ui/src/components/View/services/login_service/LoginForm.vue
+++ pranama-ui/src/components/View/services/login_service/LoginForm.vue
@@ -7,10 +7,10 @@
           <div class="col-6 col-12-xsmall">
             <input
               type="text"
-              name="username"
-              value="User Name"
-              placeholder="User Name"
-              v-model="username"
+              name="email"
+              value="Your Email"
+              placeholder="Your Email"
+              v-model="email"
             />
           </div>
           <div class="col-6 col-12-xsmall"></div>
@@ -49,67 +49,6 @@
         <!-- <div   v-show="firstName"></div> -->
       </div>
     </form>
-
-    <!-- hidden Div Starts -->
-    <div v-show="sendingSuccessful">
-      <!-- <h3>Pranama</h3> -->
-      <div class="box">
-        <p>
-          We received your email, our team will reach out to you as soon as possible . Felis sagittis eget tempus primis in faucibus vestibulum.
-          Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt felis sagittis eget.
-          tempus euismod. Magna sed etiam ante ipsum primis in faucibus vestibulum.
-        </p>
-      </div>
-      <!-- Section -->
-      <section v-show="sendingSuccessful">
-        <header class="major">
-          <h2>What we do</h2>
-        </header>
-        <div class="features">
-          <article>
-            <span class="icon fa-gem"></span>
-            <div class="content">
-              <h3>Portitor ullamcorper</h3>
-              <p>
-                Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-                facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.
-              </p>
-            </div>
-          </article>
-          <article>
-            <span class="icon solid fa-paper-plane"></span>
-            <div class="content">
-              <h3>Sapien veroeros</h3>
-              <p>
-                Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-                facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.
-              </p>
-            </div>
-          </article>
-          <article>
-            <span class="icon solid fa-rocket"></span>
-            <div class="content">
-              <h3>Quam lorem ipsum</h3>
-              <p>
-                Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-                facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.
-              </p>
-            </div>
-          </article>
-          <article>
-            <span class="icon solid fa-signal"></span>
-            <div class="content">
-              <h3>Sed magna finibus</h3>
-              <p>
-                Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-                facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.
-              </p>
-            </div>
-          </article>
-        </div>
-      </section>
-    </div>
-    <!-- hidden Div Ends -->
   </div>
 </template>
 <script>
@@ -121,12 +60,6 @@ export default {
     return {
       sendingSuccessful: false,
       email: "",
-      phoneNumber: "",
-      firstName: "",
-      lastName: "",
-      message: "",
-      text: "sent",
-      username: "",
       password: "",
       error: false,
       processing: false
@@ -140,7 +73,7 @@ export default {
       login: "auth/login",
     }),
     reset() {
-      this.username = "";
+      this.email = "";
       this.password = "";
       this.error = false;
     },
@@ -149,7 +82,7 @@ export default {
       this.processing = true;
       try {
         await this.login({
-          username: this.username,
+          email: this.email,
           password: this.password
         });
         this.sendingSuccessful = true;

e587d0b480ae4cfda4efae27d6f0a645e247fb44
Escuela Technologies <escuela.tech@gmail.com>
2020-06-01 20:37:33 -0500

    Modified the homepage

--- pranama-ui/src/components/View/services/dashboard_service/dashboard.vue
+++ pranama-ui/src/components/View/services/dashboard_service/dashboard.vue
@@ -1,5 +1,4 @@
 <template>
-    
     <div>
     <!-- Wrapper -->
     <div id="wrapper">
@@ -10,12 +9,10 @@
           <Header />
           <!-- Header -->
           <!-- Form -->
-         
           <section>
 					<!-- <header class="main">
 						<h1>Elements</h1>
 					</header> -->
-
 					<!-- Content -->
 					<h2 id="content">How it works </h2>
 					<p>If have a requirement to take care of your Nears & Dears in India, Kerala, Kozhikode you can go
@@ -46,93 +43,7 @@
 							<input type="button"  value="Our Offered Services" class="primary"  @click="$router.push({name: 'OurOfferedServices'})" />
 						</div>
 					</div>
-					<!-- <div class="row"> -->
-						<!-- <div class="col-6 col-12-small">
-							<h3>Sem turpis amet semper</h3>
-							<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi
-								integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat commodo eu sed
-								ante lacinia. Sapien a lorem in integer ornare praesent commodo adipiscing arcu in massa
-								commodo lorem accumsan at odio massa ac ac. Semper adipiscing varius montes viverra nibh
-								in adipiscing blandit tempus accumsan.</p>
-						</div>
-						<div class="col-6 col-12-small">
-							<h3>Magna odio tempus commodo</h3>
-							<p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet. Accumsan ac
-								integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor. Ante
-								commodo blandit adipiscing integer semper orci eget. Faucibus commodo adipiscing mi eu
-								nullam accumsan morbi arcu ornare odio mi adipiscing nascetur lacus ac interdum morbi
-								accumsan vis mi accumsan.</p>
-						</div> -->
-						<!-- Break -->
-						<!-- <div class="col-4 col-12-medium">
-							<h3>Ayurveda Treatment</h3>
-							<p>Ayurveda is the 5th Veda needed for health, the traditional natural, healing system, 
-                practiced in India for more than 5000 years. Ayurveda derives the name from the Sanskrit words, “ayur” “veda”. 
-                Ayur means life and longevity. So Ayurveda is the science of Healthy life. For healthy life we need balanced diet, healthy lifestyle and harmonious mind Ayurvedic Therapies Panchakarma We are associated with few Ayurveda hospitals for Rejnuvation treatments like Panchakarma. 
-                If needed treatment options also can arranged.</p>
-						</div>
-						<div class="col-4 col-12-medium">
-							<h3>Patient Pick up</h3>
-							<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi
-								integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante
-								lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing blandit.</p>
-								<input type="button"  value="Patient Pickup" class="primary"  @click="$router.push({name: 'PatientPickupPage'})" />
-						</div>
-
-						
-              
-            
-						<div class="col-4 col-12-medium">
-							<h3>Hospital Assistance</h3>
-							<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi
-								integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante
-								lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing blandit.</p>
-								<input type="button"  value="Hospital Assistance" class="primary"  @click="$router.push({name: 'HospitalAssistancePage'})" />
-						</div> -->
-					<!-- </div> -->
 				</section>
-                <!-- Section 
-				<section>
-					<header class="major">
-						<h2>Erat lacinia</h2>
-					</header>
-					<div class="features">
-						<article>
-							<span class="icon fa-gem"></span>
-							<div class="content">
-								<h3>Portitor ullamcorper</h3>
-								<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-									facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							</div>
-						</article>
-						<article>
-							<span class="icon solid fa-paper-plane"></span>
-							<div class="content">
-								<h3>Sapien veroeros</h3>
-								<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-									facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							</div>
-						</article>
-						<article>
-							<span class="icon solid fa-rocket"></span>
-							<div class="content">
-								<h3>Quam lorem ipsum</h3>
-								<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-									facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							</div>
-						</article>
-						<article>
-							<span class="icon solid fa-signal"></span>
-							<div class="content">
-								<h3>Sed magna finibus</h3>
-								<p>Aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin aliquam
-									facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
-							</div>
-						</article>
-					</div>
-				</section> -->
-        
-				
           <!-- Form -->
         </div>
       </div>
@@ -143,8 +54,6 @@
       <!-- Sidebar -->
     </div>
     </div>
-
-    
 </template>
 
 <script>
--- pranama-ui/src/components/View/services/landing_page/PranamaHomePage.vue
+++ pranama-ui/src/components/View/services/landing_page/PranamaHomePage.vue
@@ -11,13 +11,52 @@
           <section id="banner">
             <div class="content">
               <header>
-                <h2>
-                  Parental Care
-                  <br />by PRANAMA
-                </h2>
-                <p>TAKING CARE OF YOUR AGEING PARENTS IS A NOBLE MOVE</p>
+                <h2> Parental Care
+                  <br> by PRANAMA</h2>
+                <p>Taking care of your aging parents is a noble move .</p>
+
               </header>
-              <p>Caring Your Aging Parents Is not easy in Todays Busy Schedule of life, that too in the other side of the globe America. The distance, time, leave availability and the expenses are the hurdles that stay in between your love to your parents and discharging your responsibilities of the care and attention you would love to give them. On their health and treatment side we offer total solution. We take care of them from home to hospital to home, hospital stay, and after discharge services, the way you would do, if you are physically present. You will get daily reports which includes videos, photos, feedback’s etc. Your parents will not feel your absence. You can be happy for give them your best to all they have done for you...</p>
+              <!-- p>Caring Your Aging Parents Is not easy in Todays Busy Schedule of life, that too in the other side 
+                of the globe America. The distance, time, leave availability and the expenses are the hurdles that 
+                stay in between your love to your parents and discharging your responsibilities of the care and 
+                attention you would love to give them. On their health and treatment side we offer total solution. 
+                We take care of them from home to hospital to home, hospital stay, and after discharge services, 
+                the way you would do, if you are physically present. 
+                You will get daily reports which includes videos, photos, feedback’s etc. 
+                Your parents will not feel your absence. 
+                You can be happy for give them your best to all they have done for you...</p -->
+              <p>
+                 Door Through Door Assisted Ride Service:    
+                 Pranama provides flexible transportation options to meet the needs of our clients. 
+                 Our assisted ride transportation is provided with door-through-door service by our Driver 
+                 Companions in a comfortable senior-friendly vehicle.   
+                 Tailored Service:  Because of the extensive training of our Driver Companions and the technology 
+                 we use to support them, SilverRide is able to deliver highly tailored service to our clients 
+                 based on their unique needs.  Whatever condition they have, 
+                 we are prepared to help based on the training drivers have already received, 
+                 and the materials they have at their fingertips with the apps we provide them.   
+                 Best Insurance in the Industry:  SilverRide maintains full $1M primary liability 
+                 insurance for all vehicles whether company-owned or part of ourdriver partner fleet.  
+                 For our rideshare drivers, this include $1M of coverage for all three periods of the TNC rules, 
+                 and include physical damage coverage for the drivers' vehicles (we're looking out for them too!). 
+                Each rideshare driver also gets a SilverRide insurance card for their vehicle, 
+                so they never have to use their own personal insurance in an accident.  
+                In addition, the Company also maintains insurance for all incidents that can happen OUTSIDE of the 
+                vehicle.  So when we take our clients out for walks in the park, to doctor's appointments -- 
+                anywhere they are going -- clients are covered from the moment we get them from 
+                in their home to inside wherever they are going. </p>
+
+               <!-- On Time Guarantee* : The Pranama On Time Guarantee states that we will arrive within 15 minutes of 
+                the scheduled departure time or the ride is half-price.   
+                Cashless Transactions: All pricing is quoted in advance and once completed, 
+                trips are automatically billed to a credit card on file or deducted from a pre-paid balance. 
+                Our Driver Companions don't accept tips, so there is never a need to carry cash.   
+                Safety: All Pranama Driver Companions have clean driving records and have gone through extensive 
+                background checks prior to beginning service with SilverRide. 
+                All Driver Companions are fingerprinted, background-checked and drug tested. 
+
+              </p> -->
+
               <p><a href="#" v-show="!displayReadMore" @click="readMore">Read More</a><span v-show="displayContent">Caring Your Aging Parents Is not easy in Todays Busy Schedule of life, that too in the other side of the globe America. The distance, time, leave availability and the expenses are the hurdles that stay in between your love to your parents and discharging your responsibilities of the care and attention you would love to give them. On their health and treatment side we offer total solution. We take care of them from home to hospital to home, hospital stay, and after discharge services, the way you would do, if you are physically present. You will get daily reports which includes videos, photos, feedback’s etc. Your parents will not feel your absence. You can be happy for give them your best to all they have done for you...</span></p>
               <!-- <ul class="actions">
                 <li>

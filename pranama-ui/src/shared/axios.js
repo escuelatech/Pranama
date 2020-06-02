63c3959e3cce0c67a7c23e2b5fb2b3475e2064cf
Escuela Technologies <escuela.tech@gmail.com>
2020-06-01 22:16:39 -0500

    Setting email to the request headers

--- pranama-ui/src/shared/axios.js
+++ pranama-ui/src/shared/axios.js
@@ -6,14 +6,16 @@ const apiClient = axios.create({
   withCredentials: false,
   headers: {
      Accept: "application/json",
-    "Content-Type": "application/json",
+             "Content-Type": "application/json",
     }
 });
 
 apiClient.interceptors.request.use(function (config) {
   let token = JSON.parse(localStorage.getItem('token'));
+  let email = JSON.parse(localStorage.getItem('email'));
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+    config.headers.email = `${email}`;
   }
   return config;
 }, function (err) {
